83d8dfde2395fa9f6d4d1c00e9473aae68e15822
Gregor Cox <gregorcox@gmail.com>
2024-08-14 12:34:47 +0100

    build basic navbar component

---
+++ app/NavBar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import classnames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavBar = () => {
+  return (
+    <div className="navbar flex p-5 border-b-2 border-white bg-gray-800">
+      <div className="flex-1">
+        <Link href="/" className="text-2xl uppercase">
+          Gregor Cox
+        </Link>
+      </div>
+      <NavLinks />
+    </div>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ];
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map(({ href, label }) => (
+        <li key={href}>
+          <Link
+            className={classnames({
+              "nav-link": true,
+              "!text-white": href === currentPath,
+            })}
+            href={href}
+          >
+            {label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavBar;
