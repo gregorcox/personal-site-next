e414d3718f13440e6349fc7319417b7492055fd3
Gregor Cox <gregorcox@gmail.com>
2024-08-15 10:56:55 +0100

    make nav bar responsive

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,59 +3,81 @@
 import classnames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
-const NavBar = () => {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const NavLinks = () => {
+    const currentPath = usePathname();
+
+    const links = [
+      {
+        label: "Home",
+        href: "/",
+      },
+      {
+        label: "About",
+        href: "/about",
+      },
+      {
+        label: "Projects",
+        href: "/projects",
+      },
+      {
+        label: "Contact",
+        href: "/contact",
+      },
+    ];
+
+    return (
+      <ul className="flex flex-col items-end space-x-6 md:flex-row">
+        {links.map(({ href, label }) => (
+          <li key={href}>
+            <Link
+              className={classnames({
+                "nav-link": true,
+                "!text-white": href === currentPath,
+              })}
+              href={href}
+              onClick={() => isOpen && setIsOpen(false)}
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <div className="navbar flex p-5 border-b-2 border-white bg-gray-800">
-      <div className="flex-1">
-        <Link href="/" className="text-2xl uppercase text-white">
-          Gregor Cox
-        </Link>
-      </div>
-      <NavLinks />
-    </div>
-  );
-};
-
-const NavLinks = () => {
-  const currentPath = usePathname();
-
-  const links = [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Contact",
-      href: "/contact",
-    },
-  ];
-
-  return (
-    <ul className="flex space-x-6">
-      {links.map(({ href, label }) => (
-        <li key={href}>
-          <Link
-            className={classnames({
-              "nav-link": true,
-              "!text-white": href === currentPath,
-            })}
-            href={href}
-          >
-            {label}
+    <nav className="flex text-white p-5 border-b-2 border-white bg-gray-800">
+      <div className="container mx-auto flex justify-between items-start md:items-center">
+        <div className="flex-1">
+          <Link href="/" className="text-2xl uppercase">
+            Gregor Cox
           </Link>
-        </li>
-      ))}
-    </ul>
+        </div>
+        <div className="flex flex-col items-end">
+          <button
+            className="focus:outline-none md:hidden"
+            onClick={() => setIsOpen(!isOpen)}
+            type="button"
+          >
+            <RxHamburgerMenu style={{ fontSize: "1.5rem" }} />
+          </button>
+          <div
+            className={`md:flex md:items-center ${
+              isOpen ? "flex flex-col items-end" : "hidden"
+            }`}
+          >
+            <NavLinks />
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
-export default NavBar;
+export default Navbar;
