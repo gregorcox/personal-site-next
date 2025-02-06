bbdbbb31544e8d267396548aff243228fee5e847
Gregor Cox <gregorcox@gmail.com>
2025-02-06 17:55:15 +0000

    style mobile nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import classnames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { MdClose } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const NavLinks = () => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => document.body.classList.remove("overflow-hidden");
+  }, [isOpen]);
+
+  const NavLinks = ({ isMobile = false }) => {
     const currentPath = usePathname();
 
     const links = [
@@ -32,16 +42,20 @@ const Navbar = () => {
     ];
 
     return (
-      <ul className="flex flex-col items-end space-x-6 md:flex-row">
+      <ul
+        className={classnames("flex", {
+          "flex-col items-center space-y-6": isMobile,
+          "md:flex-row md:space-x-6 hidden md:flex": !isMobile,
+        })}
+      >
         {links.map(({ href, label }) => (
           <li key={href}>
             <Link
-              className={classnames({
-                "nav-link": true,
+              className={classnames("nav-link", {
                 "!text-white": href === currentPath,
               })}
               href={href}
-              onClick={() => isOpen && setIsOpen(false)}
+              onClick={() => isMobile && setIsOpen(false)}
             >
               {label}
             </Link>
@@ -53,29 +67,33 @@ const Navbar = () => {
 
   return (
     <nav className="flex text-white p-5 border-b-2 border-white bg-gray-800">
-      <div className="container mx-auto flex justify-between items-start md:items-center">
-        <div className="flex-1">
-          <Link href="/" className="text-2xl uppercase">
-            Gregor Cox
-          </Link>
-        </div>
-        <div className="flex flex-col items-end">
-          <button
-            className="focus:outline-none md:hidden"
-            onClick={() => setIsOpen(!isOpen)}
-            type="button"
-          >
-            <RxHamburgerMenu style={{ fontSize: "1.5rem" }} />
-          </button>
-          <div
-            className={`md:flex md:items-center ${
-              isOpen ? "flex flex-col items-end" : "hidden"
-            }`}
-          >
-            <NavLinks />
-          </div>
-        </div>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-2xl uppercase">
+          Gregor Cox
+        </Link>
+
+        <NavLinks />
+
+        <button
+          className="focus:outline-none md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+          type="button"
+        >
+          <RxHamburgerMenu style={{ fontSize: "1.5rem" }} />
+        </button>
       </div>
+
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 z-50">
+          <button
+            className="absolute top-6 right-4 text-white"
+            onClick={() => setIsOpen(false)}
+          >
+            <MdClose style={{ fontSize: "2rem" }} />
+          </button>
+          <NavLinks isMobile />
+        </div>
+      )}
     </nav>
   );
 };
